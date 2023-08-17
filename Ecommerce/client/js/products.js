b577a999136fc186ba5189cc40558fcd96d7a6dc
Valentin Albornoz <valeg486@gmail.com>
2023-08-16 23:05:02 -0300

    Initial commit

---
+++ Ecommerce/client/js/products.js
@@ -0,0 +1,31 @@
+const productos = [
+  {
+    id: "1",
+    productName: "iPhone 13 Pro Max",
+    price: 999,
+    quanty: 1,
+    image: "../media/iphone-14-pro.jpg",
+  },
+  {
+    id: "2",
+    productName: "MacBook Pro",
+    price: 1999,
+    quanty: 1,
+    image: "../media/macpro.jpg",
+  },
+  {
+    id: "3",
+    productName: "iPad Air",
+    price: 699,
+    quanty: 1,
+    image: "../media/ipad-air.jpg",
+  },
+  {
+    id: "4",
+    productName: "Apple Watch Series 7",
+    price: 500,
+    quanty: 1,
+    image: "../media/applewatch.jpg",
+  },
+];
+
